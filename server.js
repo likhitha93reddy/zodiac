d427e9ecf947c90920da8599c39b059149679247
likhitha <reddylikhitha483@gmail.com>
2023-09-22 23:45:44 +0530

    few changes made

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 app.use(express.static(__dirname + '/client'));
-
-const port = 3000;
+//setup Environment variable
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-	console.log('Server is running nodemon on the port'+port);
+	console.log('Server is running on the port'+port);
 });
